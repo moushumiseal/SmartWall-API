37f5bea668d3db88c3cfc90979ac840f31c24fe0
DhruvMG317 <e0146953@u.nus.edu>
2018-01-09 12:42:35 +0800

    bus

--- SmartWall/db/smartwall_test_schema_BusStop.sql
+++ SmartWall/db/smartwall_test_schema_BusStop.sql
@@ -3,8 +3,9 @@ DROP TABLE IF EXISTS `BusStop`;
 CREATE TABLE `BusStop` (
   `id` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
+  `bus` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-insert into smartwall_test_schema.BusStop values (1,"Kent Ridge"),(2,"UTown"),(3,"Library");
+insert into smartwall_test_schema.BusStop values (1,"Kent Ridge","A2"),(2,"UTown","D1"),(3,"Computer Centre","D1"),(4,"UTown","D1"),(5,"Computer Centre","D1");
--- SmartWall/src/java/edu/sg/nus/iss/smartwall/business/BusStopBean.java
+++ SmartWall/src/java/edu/sg/nus/iss/smartwall/business/BusStopBean.java
@@ -5,6 +5,10 @@
  */
 package edu.sg.nus.iss.smartwall.business;
 
+import edu.sg.nus.iss.smartwall.model.Bus;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import edu.sg.nus.iss.smartwall.model.BusStop;
 import java.util.List;
 import javax.ejb.Stateless;
@@ -39,4 +43,36 @@ public class BusStopBean  {
         return (query.setParameter("name",name).getResultList());
     }
     
+    public List<Bus> findBusByName(String name){
+        LocalDateTime now;
+        now= LocalDateTime.now();
+        String t_now,t_max;
+        t_now= DateTimeFormatter.ofPattern("HH:mm:ss").format(now);
+        t_max=shift(t_now);
+        Time tn,tm;
+        tn=Time.valueOf(t_now);
+        tm=Time.valueOf(t_max);
+        TypedQuery<Bus> query = em.createQuery("select t from Bus t where t.name = :name and t.time >= '"+tn.toString() +"' and t.time<= '"+tm.toString() +"'", Bus.class);
+        return (query.setParameter("name",name).getResultList());
+    }
+    public String shift(String in)
+    {
+        String res="";
+        int h,m;
+        h=Integer.parseInt(in.substring(0, 2));
+        m=Integer.parseInt(in.substring(3,5));
+        if(m<30)
+            m+=30;
+        else
+        {
+            h+=1; 
+            m=m+30;
+            m=m%60;
+        }
+        if(m<10)
+            res+=h+":0"+m+":"+in.substring(6);
+        else
+        res+=h+":"+m+":"+in.substring(6);
+        return res;
+    } 
 }
